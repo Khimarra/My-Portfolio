a2f5b6468919d6765d5b4c27da05ecb99a53f399
Tara Solbrig <tsolbrig87@gmail.com>
2020-03-22 15:57:35 -0400

    styled projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,14 @@ const About = () => {
             <h1 className='section-title'>About</h1>
             <img src={headshot} alt='headshot'></img>
             <p>I am a highly creative, pragmatic, and passionate engineer with 4 years’ experience working in management of a craft retail company, and almost 10 years’ experience as a professional artist. I believe I have found the artistry in coding, and seek to apply my imaginative, out-of-the-box thinking to new programming challenges. I strive to work in a collaborative environment that embraces innovative design, and encourages constant learning.</p>
+            <div className='fa-links'>
+                <a className="icons" href="https://www.linkedin.com/in/tarasolbrig/">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+                <a  className="icons" href="https://github.com/Khimarra">
+                    <i className="fab fa-github-square"></i>
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/stylesheets/projects.css'
 
 const Projects = () => {
 
@@ -6,57 +7,79 @@ const Projects = () => {
         {
             title: 'Techwitch Tarot',
             url: 'https://pages.git.generalassemb.ly/Khimarra/Techwitch-Tarot/',
+            github: 'https://git.generalassemb.ly/Khimarra/Techwitch-Tarot',
             screenshot: 'https://i.imgur.com/BSt1vYN.jpg',
             languages: [
                 'JavaScript', 'HTML', 'CSS'
             ],
-            APIs: 'https://rws-cards-api.herokuapp.com/'
+            APIs: 'https://rws-cards-api.herokuapp.com/',
+            description: 'this is a description of my tarot app'
         },
         {
             title: 'The Intermet',
             url: 'http://the-intermet.surge.sh/',
+            github: 'https://git.generalassemb.ly/Khimarra/The-InterMet',
             screenshot: 'https://i.imgur.com/UqOSWyr.jpg',
             languages: [
                 'React', 'CSS'
             ],
-            APIs: 'https://www.metmuseum.org/blogs/now-at-the-met/2018/met-collection-api'
+            APIs: 'https://www.metmuseum.org/blogs/now-at-the-met/2018/met-collection-api',
+            description: 'React app built using a RESTful API to retreive and render info from the Met Collection.'
         },
         {
             title: 'NJTransit Re-design',
             url: 'https://nj-transit-mobile.herokuapp.com/',
+            github: '',
             screenshot: 'https://i.imgur.com/7yWWpki.jpg',
             languages: [
                 'React', 'Express', 'Sequelize', 'CSS'
             ],
-            APIs: ''
+            APIs: '',
+            description: 'Group project, full-stack mobile site, based on wireframes provided by UX designers tasked with re-designing the existing NJTransit mobile site.'
         },
         {
             title: 'Bspoke',
             url: 'http://bspoke-app.herokuapp.com/',
+            github: 'https://git.generalassemb.ly/Khimarra/Bspoke',
             screenshot: 'https://i.imgur.com/F1mNU6x.jpg',
             languages: [
                 'React', 'Ruby on Rails', 'SASS'
             ],
-            APIs: ''
+            APIs: '',
+            description: 'A full-stack app with user authentication and full CRUD capabilities. Users can create an account and display their own art.'
         }
     ]
 
     return (
-        <div>
+        <div className='projects'>
             <a id='projects'><h1 className='section-title'>Projects</h1></a>
-            <div className='project-card'>
+            <div>
                 {allProjects.map((project, index) => {
-                    return(
+                    return (
                         <div className='project-card'>
                             <h2>{project.title}</h2>
-                            <img src={project.screenshot}></img>
-                            {project.languages.map((language, index) => {
-                                return(
-                                    <h4>{language}</h4>
-                                )
-                            })}
+                            <div className='image-container'>
+                                <img src={project.screenshot}></img>
+                            </div>
+                            <div className='languages'>
+                                {project.languages.map((language, index) => {
+                                    return (
+                                        <h4>#{language}</h4>
+                                    )
+                                })}
+                                
+                            </div>
+
+                            <div className='fa-links'>
+                                <a className='icons' href={`${project.url}`}>
+                                    <i class="fas fa-link"></i>
+                                </a>
+                                <a className='icons' href={`${project.github}`}>
+                                    <i className="fab fa-github-square"></i>
+                                </a>
+                            </div>
+
                         </div>
-                            
                     )
                 })}
             </div>
