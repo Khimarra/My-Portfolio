ed939583b5728a34f0ef0c994c24f2fb82eeaaec
Tara Solbrig <tsolbrig87@gmail.com>
2020-03-22 19:52:54 -0400

    added languages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 import headshot from '../images/headshot.jpg'
 import '../styles/stylesheets/about.css'
 
+const allLanguages = [
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'SASS',
+    'Node.js',
+    'React',
+    'npm',
+    'Git',
+    'PostgreSQL',
+    'Slack',
+    'Express',
+    'AWS',
+    'Ruby',
+    'Jira',
+    'Rails',
+    'Asana'
+]
+
 const About = () => {
     return (
         <div className='about'>
@@ -9,6 +28,15 @@ const About = () => {
             <div className='headshot-container'>
                 <img src={headshot} alt='headshot'></img>
             </div>
+            <div className='languages about-languages'>
+                {allLanguages.map((project, index) => {
+                    return(
+                        <div>
+                            <h4>#{project}</h4>
+                        </div>
+                    )
+                })}
+            </div> 
             <p>I am a highly creative, pragmatic, and passionate engineer with 4 years’ experience working in management of a craft retail company, and almost 10 years’ experience as a professional artist. I believe I have found the artistry in coding, and seek to apply my imaginative, out-of-the-box thinking to new programming challenges. I strive to work in a collaborative environment that embraces innovative design, and encourages constant learning.</p>
             <div className='fa-links'>
                 <a className="icons" href="https://www.linkedin.com/in/tarasolbrig/">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../styles/stylesheets/contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            Hello from Contact
+        <div className='contact'>
+            <a id='contact'><h1>Contact</h1></a>
+            <div className='form-container'>
+                <form action="https://formspree.io/xpzlojyv" method="POST">
+                    <h3>Name:</h3>
+                    <input type="text" name="name" placeholder='name'></input>
+                    <h3>Email:</h3>
+                    <input type="email" name="_replyto" placeholder='email'></input>
+                    <h3>Message:</h3>
+                    <textarea type='text' name='message' rows='7' cols='30' placeholder='send me words'></textarea>
+                    <input className='button' type="submit" value="Send"></input>
+                </form>
+            </div>
         </div>
     )
 }
