426bc9281af3b62697fb3eef9e62d4b5fb765af0
Tara Solbrig <tsolbrig87@gmail.com>
2020-03-29 19:30:12 -0400

    fixed home link and added pointer cursor on cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import '../styles/stylesheets/home.css'
 const Home = () => {
     return (
         <div className='home-container'>
-            <div className='bg-image'></div>
             <a id='home'></a>
+            <div className='bg-image'></div>
             <h1 className='hello'>Hi, I'm <span>Tara Solbrig</span></h1>
             <h1 className='mini-bio'>I'm a full-stack web developer and software engineer</h1>
         </div>
